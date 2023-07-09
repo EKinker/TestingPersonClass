1b12d220260fe6500ce62e5a8a5dfe549ca035cc
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-09 18:44:38 -0400

    mostly

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,51 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private boolean isAlive;
 
-    public Person() {
+    public Person() {  //these are the defaults
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        this.isAlive = true;
+
+
+
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
+    public Person(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
+
+    public Boolean isAlive() { return isAlive;}
 }
